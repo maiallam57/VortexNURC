2d7be68b1e3318940774ac87fddcd0e11f94f968
maiallam57 <maimallam57@gmail.com>
2024-06-01 22:07:42 +0300

    Fixed Bugs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
+var myNav = document.getElementById("nav");
+var myLogo = document.getElementById("logo");
+
 var darkIcon = document.getElementById("darkIcon");
 var lightIcon = document.getElementById("lightIcon");
 var body = document.getElementById("body");
+
+
+function onScroll() {
+
+    if ((document.body.scrollTop>= 0)  || (document.documentElement.scrollTop >= 0)) {
+        myNav.classList.add("navbar-light");
+        myNav.classList.remove("navbar-dark");
+        myLogo.classList.remove("logo-light");
+        myLogo.classList.add("logo-dark");
+    } else {
+        myNav.classList.add("navbar-dark");
+        myNav.classList.remove("navbar-light");
+        myLogo.classList.add("logo-light");
+        myLogo.classList.remove("logo-dark");
+    }
+}
 
 function lightMode(){
     lightIcon.classList.add("d-none");
@@ -14,3 +33,7 @@ function darkMode(){
     lightIcon.classList.remove("d-none");
     body.classList.add("dark");
 }
+
+onScroll();
+
+window.onscroll = onScroll;
